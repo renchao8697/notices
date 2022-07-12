daef833ade84e75db4bc782cc26fe51b1b2f4745
renchao8697 <renchao8697@sina.com>
2022-07-12 17:23:49 +0800

    添加navbar，sidebar

---
+++ docs/.vuepress/configs/navbar.ts
@@ -0,0 +1,42 @@
+import type { NavbarConfig } from "vuepress";
+
+export const navbar: NavbarConfig = [
+  {
+    text: '前端基础',
+    children: [
+      '/frontend-base/html.md',
+      '/frontend-base/css.md',
+      '/frontend-base/javascript/'
+    ]
+  },
+  {
+    text: '框架',
+    children: [
+      {
+        text: 'Vue',
+        children: [
+          '/frame/vue2/',
+          '/frame/vue3/',
+        ]
+      },
+      {
+        text: 'React',
+        children: [
+          '/frame/react/',
+        ]
+      },
+      {
+        text: 'Webpack',
+        children: [
+          '/frame/webpack/',
+        ]
+      }
+    ]
+  },
+  {
+    text: 'HTTP',
+    children: [
+      '/http/cache'
+    ]
+  }
+]
---
+++ docs/.vuepress/configs/sidebar.ts
@@ -0,0 +1,29 @@
+import type { SidebarConfig } from "vuepress";
+
+export const sidebar: SidebarConfig = {
+  '/frontend-base/': [
+    '/frontend-base/html.md',
+    '/frontend-base/css.md',
+    {
+      text: 'Javascript',
+      children: [
+        '/frontend-base/javascript/index.md',
+        '/frontend-base/javascript/proxy.md',
+      ]
+    }
+  ],
+  '/frame/': [
+    {
+      text: 'Vue',
+      children: [
+        '/frame/vue2/',
+        '/frame/vue3/'
+      ]
+    },
+    '/frame/react/',
+    '/frame/webpack/',
+  ],
+  '/http/': [
+    '/http/cache.md'
+  ]
+}
