ce569d611d11dcc401b921b75f2e59fb8f1b9884
renchao8697 <renchao8697@sina.com>
2022-07-12 17:33:07 +0800

    添加docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,20 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+import { head, navbar, sidebar } from './configs'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'Note',
   description: '学习笔记',
-  base: '/note/'
+  base: '/note/',
+  head,
+  theme: defaultTheme({
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        navbar: navbar,
+        sidebar: sidebar
+      }
+    }
+  })
 })
