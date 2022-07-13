57aeb739e1de2f0150104675cbb91abfd039e438
renchao8697 <renchao8697@sina.com>
2022-07-13 11:25:40 +0800

    添加搜索、编辑

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
-import { defaultTheme, defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress';
+import { searchPlugin } from '@vuepress/plugin-search';
 
-import { head, navbar, sidebar } from './configs'
+import { head, navbar, sidebar } from './configs';
 
 export default defineUserConfig({
   lang: 'zh-CN',
@@ -9,12 +10,21 @@ export default defineUserConfig({
   base: '/note/',
   head,
   theme: defaultTheme({
+    docsRepo: 'https://gitlab.com/renchao8697/note',
+    docsBranch: 'main',
     docsDir: 'docs',
-    locales: {
-      '/': {
-        navbar: navbar,
-        sidebar: sidebar
-      }
-    }
-  })
-})
+    editLinkPattern: ':repo/-/edit/:branch/:path',
+    navbar: navbar,
+    sidebar: sidebar,
+  }),
+  plugins: [
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: 'search',
+        },
+      },
+      getExtraFields: (page) => (page.frontmatter.tags as []) ?? [],
+    }),
+  ],
+});
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,3 @@
-# VuePress
+# Note
 
 学习笔记
